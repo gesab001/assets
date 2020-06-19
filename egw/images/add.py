43e426e61e63bf7ab4b3c6b927ce08d83691d2fc
gesab001 <144000giovanni@gmail.com>
2020-06-20 02:53:53 +1200

    added script for adding images

---
+++ egw/images/add.py
@@ -0,0 +1,31 @@
+import os
+import json
+
+f = open("imageslist.json")
+jsondata = json.load(f) 
+f.close()
+arr = os.listdir()
+
+for x in arr:
+   isDirectory = os.path.isdir(x)
+   if isDirectory:
+      if x not in jsondata:
+       jsondata[x] = []
+      files = os.listdir(x)
+      for file in files:
+          url = 'https://gesab001.github.io/assets/egw/images/' + x + "/" + file  
+          if url in jsondata[x]:
+             print(x + ": exists")  
+          else:
+             print("not exists")
+             url = 'https://gesab001.github.io/assets/egw/images/' + x + "/" + file  
+             jsondata[x].append(url)
+
+
+newimage = input("url : " )
+folder = input("folder: " )
+if newimage not in jsondata[folder]:   
+   jsondata[folder].append(newimage)
+
+with open("imageslist.json", "w") as outfile:
+    json.dump(jsondata, outfile)
