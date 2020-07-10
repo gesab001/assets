e1ab9f60d04624df3a7dcb5a1495c05db40c45ae
gesab001 <144000giovanni@gmail.com>
2020-07-11 03:56:19 +1200

    added story

---
+++ story/add.py
@@ -0,0 +1,41 @@
+import json
+
+f = open("stories.json")
+jsondata = json.load(f)
+f.close()
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+
+def addStory(title, letter):
+  print(title)
+  print(letter)
+  f = open("stories.json")
+  jsondata = json.load(f)
+  f.close()
+  for i in range(0, 26):
+     if jsondata[i]["letter"]==letter:
+         jsondata[i]["names"].append(title)
+  print(jsondata)
+  with open("stories.json", "w") as outfile:
+     json.dump(jsondata, outfile)
+def getLettersList():
+  result = list(alphabets)
+  return result
+  
+def clearDatabase():
+  jsondata = []
+  letterslist = getLettersList()
+  for l in letterslist:
+    print(l.capitalize())
+    letter = l.capitalize()
+    obj = {"letter": letter, "names":[]}
+    jsondata.append(obj)
+  with open("stories.json", "w") as outfile:
+    json.dump(jsondata, outfile)
+
+#clearDatabase()
+
+
+while True:
+   newstory = input("story title: " )
+   letter = newstory[0:1].upper()
+   addStory(newstory.capitalize(), letter)
