58bb746a1d6f547cbccf008f93b30340866a2648
gesab001 <144000giovanni@gmail.com>
2020-07-18 08:12:18 +1200

    update

--- images/downloadimage.py
+++ images/downloadimage.py
@@ -1,6 +1,26 @@
 import subprocess
 
-url = input("url: " )
-filename = input("filename: " )
-command = "curl -L " + url + " -o " + filename
-subprocess.call(command, shell=True)
+folder = input("folder: " )
+
+def resize(filename):
+    command = "identify -format " + "%wx%h "
+    p = subprocess.Popen(['identify', '-format', '%w', filename], stdout=subprocess.PIPE)
+    result = p.communicate()[0].decode("utf-8") 
+    print (result)
+    width = 710
+    height = 400
+    command = "convert "+filename+" -resize " + str(width) + "x" + str(height) + "\! " + filename
+    print(command)
+    subprocess.call(command, shell=True)
+    p = subprocess.Popen(['identify', '-format', '%wx%h', filename], stdout=subprocess.PIPE)
+    result = p.communicate()[0].decode("utf-8") 
+    print ("new size: " + result)
+
+
+while True:
+    url = input("url: " )
+    filename = input("filename: " )
+    path = "./"+folder+"/"+filename
+    command = "curl -L " + url + " -o " + path
+    subprocess.call(command, shell=True)
+    resize(path)
