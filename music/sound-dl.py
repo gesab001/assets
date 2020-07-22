d789b9872c059332a2efd63c62d08987a45f90de
gesab001 <144000giovanni@gmail.com>
2020-07-22 12:27:01 +1200

    update

--- music/sound-dl.py
+++ music/sound-dl.py
@@ -1,1 +1,5 @@
-youtube-dl -x --audio-format mp3 url --output out.mp3
+import subprocess
+
+url = input("url:")
+output = input("filename:")
+subprocess.call("youtube-dl -x --audio-format mp3 "+ url +" --output "+output+".mp3", shell=True)
