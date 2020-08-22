7fddcbcdc80b441b70706e67669362d6e95b81f0
gesab001 <144000giovanni@gmail.com>
2020-08-23 08:43:08 +1200

    update

---
+++ homeschool/math/create_questions.py
@@ -0,0 +1,41 @@
+import json
+
+f = open("math_questions.json")
+jsondata = json.load(f)
+f.close()
+
+
+def updateIndexHtml(message):
+    fileappend = open("../index.html", "a+")
+    fileappend.write("<p>added " + message + "</p>")
+    fileappend.close() 
+
+def createQuestions():
+   questions = []
+   for x in range(20):
+     data = {"question": "", "answer": "", "choices": ["", "", "", ""]}
+     question = input("question " + str(x+1) + ": " )
+     answer = input("answer: ")
+     a = answer
+     b = input("choice b: ")
+     c = input("choice c: ")
+     d = input("choice d: ")
+     data["question"] = question
+     data["answer"] = answer
+     data["choices"] = [a, b, c, d]
+     questions.append(data)
+   return questions
+
+
+
+#clearDatabase()
+
+
+titleset = input("titleset: ")
+questions = createQuestions()
+jsondata[titleset] = {"questions": []}
+jsondata[titleset]["questions"] = questions
+
+with open("math_questions.json", "w") as outfile:
+   json.dump(jsondata, outfile, indent=4)
+
