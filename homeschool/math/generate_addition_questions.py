321d72f3e768a9555a5b092d11d88a36d8e7ac46
gesab001 <144000giovanni@gmail.com>
2020-08-27 11:03:07 +1200

    update

--- homeschool/math/generate_addition_questions.py
+++ homeschool/math/generate_addition_questions.py
@@ -4,12 +4,12 @@ questions = []
 
 for x in range(0,100):
 
-   question = str(x)  + " + " +   str(1)
-   answer = x + 1
+   question = str(x)  + " + " +   str(2)
+   answer = x + 2
    a = answer
    b = x  - 1
-   c = x
-   d = x + 2
+   c = x + 1
+   d = x + 3
    choices = [a, b, c, d]
    jsondata = {"question": question, "answer": answer, "choices": choices}
    questions.append(jsondata)
