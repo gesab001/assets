086b31cca069f2235b357082106fb40a9cacda70
gesab001 <144000giovanni@gmail.com>
2020-08-27 12:07:38 +1200

    update

--- homeschool/math/generate_addition_questions.py
+++ homeschool/math/generate_addition_questions.py
@@ -1,6 +1,8 @@
 import json
 
 questions = []
+#addition 1-100 adding 0-9
+""" 
 add = int(input("add number: "))
 for x in range(0,100):
   
@@ -13,6 +15,26 @@ for x in range(0,100):
    choices = [a, b, c, d]
    jsondata = {"question": question, "answer": answer, "choices": choices}
    questions.append(jsondata)
+
+with open("sample.json", "w") as outfile:
+    json.dump(questions, outfile, indent=4)
+
+"""
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for x in range(0,100):
+   question = "L" * numbers[0]
+   answer = numbers[0]
+   a = answer
+   b = x  - (answer-1)
+   c = x + (answer+1)
+   d = x + (answer+2)
+   choices = [a, b, c, d]
+   jsondata = {"question": question, "answer": answer, "choices": choices}
+   questions.append(jsondata)
+   del numbers[0]
+   if len(numbers)==0:
+      numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 with open("sample.json", "w") as outfile:
     json.dump(questions, outfile, indent=4)
@@ -30,7 +52,7 @@ samplejson = json.load(samplefile)
 
 titleset = input("titleset: ")
 questions = samplejson
-type = "addition"
+type = "count"
 jsondata[titleset] = {"type": type, "questions": []}
 jsondata[titleset]["questions"] = questions
 
