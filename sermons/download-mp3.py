45e3af941d38e35b35dfe9c1f92236fc5089b762
gesab001 <144000giovanni@gmail.com>
2020-08-30 02:36:59 +1200

    update

--- sermons/download-mp3.py
+++ sermons/download-mp3.py
@@ -4,15 +4,17 @@ import json
 url = input("youtube url: ")
 title = input("title: ")
 preacher = input("preacher: ")
-filename = title.lower().replace(" ", "_") + preacher.lower().replace(" ", "_") + ".mp3"
+filename = title.lower().replace(" ", "_") + "_" + preacher.lower().replace(" ", "_") + ".mp3"
 command = "youtube-dl -x --audio-format mp3 --write-info-json --write-thumbnail --audio-quality 9 --embed-thumbnail " + url + " -o " + filename
 subprocess.call(command, shell=True)
 
+infofile = open(filename+".info.json")
+infodata = json.load(infofile)
 fileopen = open("sermons.json")
-jsondata = json.load(fileopen())
-jsonobj = {"title": title, "preacher": preacher, "filename": filename}
+jsondata = json.load(fileopen)
+jsonobj = {"title": title, "preacher": preacher, "filename": filename, "info": infodata, "thumbnail": filename+".jpg"}
 
-jsondata["items"].append(jsonobj)
-
-with open("sermons.json", "w") as outfile:
+if jsonobj not in jsondata["items"]:
+ jsondata["items"].append(jsonobj)
+ with open("sermons.json", "w") as outfile:
   json.dump(jsondata, outfile, indent=4)
