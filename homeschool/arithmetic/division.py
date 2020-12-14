438de149efd3c2b11ce71e4d6f5b7538a53e2083
gesab001 <144000giovanni@gmail.com>
2020-12-14 21:23:18 +1300

    changed booklist egw

--- homeschool/arithmetic/division.py
+++ homeschool/arithmetic/division.py
@@ -1,10 +1,29 @@
 import random
 
-numbers = [5,10]
-numberb = [1,2,5]
+numbers = [2,5,10]
+numbertens = [10]
+numberfives = [5]
+numbertwos = [2]
+
 for x in range(1,21):
-    b = numberb[random.randint(0, len(numberb)-1)]
-    a = numbers[random.randint(0, len(numbers)-1)]
-    print(str(x) + ".", str(a), "/", str(b))
-
-
+    b = random.randint(0, 100)
+    a = 0
+    if (b%10==0): 
+      a = 10
+      print(str(x) + ".", str(b), "/", str(a))
+    if(b%5==0):
+      if(b>51):
+          b = b%50
+      a = 5
+      print(str(x) + ".", str(b), "/", str(a))
+    if(b%2==0):
+      if (b>20):
+         b = b%20 
+      a = 2
+      print(str(x) + ".", str(b), "/", str(a))
+    if(b%3==0):
+      if (b>30):
+          
+         b = b%30 
+      a = 3
+      print(str(x) + ".", str(b), "/", str(a))         
--- homeschool/arithmetic/multiply.py
+++ homeschool/arithmetic/multiply.py
@@ -1,8 +1,8 @@
 import random
 
-numbers = [1,2,5,10]
+numbers = [1,2,3,5,10]
 for x in range(1,21):
-    b = random.randint(0, 11)
+    b = random.randint(0, 10)
     a = numbers[random.randint(0, len(numbers)-1)]
     print(str(x) + ".", str(a), "x", str(b))
 
