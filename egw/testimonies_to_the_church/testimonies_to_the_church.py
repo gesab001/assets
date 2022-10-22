860b74c333e13d2fa1937371459bac91fbbae875
giovanni <144000giovanni@gmail.com>
2022-10-23 05:57:32 +1300

    added

---
+++ egw/testimonies_to_the_church/testimonies_to_the_church.py
@@ -0,0 +1,39 @@
+
+f = open("testimonies_to_the_church.txt", "r")
+string = f.read()
+#print(string)
+import re
+import os
+import json
+
+pattern = "\{\dT \d+.\d\}"
+paraList = re.split(pattern, string)
+print(paraList)
+del paraList[-1]
+references = re.findall(pattern, string )
+#print(x)
+# ['one', 'two', 'three', 'four']
+
+print(len(paraList))
+print(len(references))
+for x in range(0, len(paraList)):
+  word = paraList[x].strip()
+  reference = references[x]
+  reference = re.sub("{", "", reference)
+  reference = re.sub("}", "", reference) 
+  print(word)
+  print(reference)
+  bookCode = reference.split(" ")[0]
+  page = reference.split(" ")[1].split(".")[0]
+  par = reference.split(" ")[1].split(".")[1]
+  print("bookCode: " + bookCode)
+  print("page: " + page)
+  print ("par: " + par)
+  index = x + 1
+  filenameBookCode = "TttC"
+  filename = "book_"+filenameBookCode+"_id_"+str(index)+".json"#book_DA_id_2122.json
+  jsonObj = {"word": word, "paragraph": int(par), "bookcode": bookCode, "page": int(page)}
+  print("filename: " + filename)
+  print(jsonObj)
+  with open("./paragraphs/"+filename, "w") as outfile:
+    json.dump(jsonObj, outfile, indent=4)
