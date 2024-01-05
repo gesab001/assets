f8f42579b913c84e537305fc035339a3db16718c
giovanni <144000giovanni@gmail.com>
2024-01-06 03:31:33 +1300

    'update'

---
+++ egw/add_egw_commentary.py
@@ -0,0 +1,32 @@
+import json
+
+f = open("egw_commentary.json", "r")
+
+jsonData = json.loads(f.read())
+print(jsonData)
+f.close()
+book = input("book: ")
+chapter = input("chapter: ")
+verse = input("verse: ")
+word = input("word: " )
+
+egw = []
+while True:
+  comment = input("egw comment: (leave blank to exit)")
+  if comment=="":
+    break
+  else:
+    proceed = input("add this comment? (y/n)")
+    if proceed.lower()=="y":
+       egw.append(comment)
+
+
+jsonData[book][chapter][verse] = {"word": word, "egw": egw}
+
+with open("egw_commentary.json", "w") as outfile:
+  json.dump(jsonData, outfile, indent=4)    
+
+verseFileName = book.replace(" ", "-") + "_"+chapter+"_"+verse+"_.json"  
+verseJsonData = {book: chapter}
+with open(verseFileName, "w") as outfile:
+  json.dump(verseJsonData, outfile, indent=4)   
