766b7bcf5807bf509eaa33e9e0a2641e55803e83
giovanni <144000giovanni@gmail.com>
2024-07-29 03:21:28 +1200

    'update'

--- egw/GC - Copy/chapters.py
+++ egw/GC - Copy/chapters.py
@@ -4,6 +4,10 @@ import os
 bookcode = "GC"
 chapterT = "The Destruction of Jerusalem"
 chapterN = 1
+startFileNumber = 25
+startPage = 17
+newPage = 39
+
 files = os.listdir()
 jsonfiles = []
 jsonfiles.sort()
@@ -13,13 +17,27 @@ for f in files:
      
 print(len(jsonfiles))
 totalfiles = len(jsonfiles)
-for x in range(1, totalfiles+1):
+
+for x in range(startFileNumber, totalfiles+1):
   filename = "book_"+bookcode+"_id_"+str(x)+".json"
   f = open(filename, "r")
   jsonobj = json.loads(f.read())
   f.close()
   page = str(jsonobj["page"])
   word = jsonobj["word"]
-  proceed = input("continue?")
-  print(word)
-  print(page)
+  #proceed = input("continue?")
+  
+  if int(page)>=startPage: 
+      if page==str(newPage):
+       break
+      else:
+       #print(word)
+       print(page)
+       print(filename)
+       jsonobj["chapter"] = chapterT
+       jsonobj["chapterN"] = chapterN     
+       print(jsonobj)     
+       with open(filename, "w") as outfile:
+          json.dump(jsonobj, outfile, indent=4)       
+  
+  
